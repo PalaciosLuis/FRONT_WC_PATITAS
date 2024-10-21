d014d6d76cd942033d9254ef52c0272a9e88602f
Luis Palacios <924333523javier@gmail.com>
2024-10-20 21:53:53 -0500

    LPS first commit

---
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc_a/controller/LoginControllerAsync.java
@@ -0,0 +1,85 @@
+package pe.edu.cibertec.patitas_frontend_wc_a.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import pe.edu.cibertec.patitas_frontend_wc_a.client.AutenticacionClient;
+import pe.edu.cibertec.patitas_frontend_wc_a.dto.LoginRequestDTO;
+import pe.edu.cibertec.patitas_frontend_wc_a.dto.LoginResponseDTO;
+import pe.edu.cibertec.patitas_frontend_wc_a.dto.LogoutRequestDTO;
+import pe.edu.cibertec.patitas_frontend_wc_a.dto.LogoutResponseDTO;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:5173")
+public class LoginControllerAsync {
+
+    @Autowired
+    WebClient webClientAutenticacion;
+
+    @Autowired
+    AutenticacionClient autenticacionClient;
+
+    @PostMapping("/autenticar-async")
+    public Mono<LoginResponseDTO> autenticar(@RequestBody LoginRequestDTO loginRequestDTO) {
+
+        // validar campos de entrada
+        if(loginRequestDTO.tipoDocumento() == null || loginRequestDTO.tipoDocumento().trim().length() == 0 ||
+            loginRequestDTO.numeroDocumento() == null || loginRequestDTO.numeroDocumento().trim().length() == 0 ||
+            loginRequestDTO.password() == null || loginRequestDTO.password().trim().length() == 0) {
+
+            return Mono.just(new LoginResponseDTO("01", "Error: Debe completar correctamente sus credenciales", "", "", "", ""));
+
+        }
+
+        try {
+
+            // consumir servicio de autenticación (Del Backend)
+            return webClientAutenticacion.post()
+                    .uri("/login")
+                    .body(Mono.just(loginRequestDTO), LoginRequestDTO.class)
+                    .retrieve()
+                    .bodyToMono(LoginResponseDTO.class)
+                    .flatMap(response -> {
+
+                        if(response.codigo().equals("00")) {
+                            return Mono.just(new LoginResponseDTO("00", "", response.nombreUsuario(), response.correoUsuario(), response.tdoc(), response.ndoc()));
+                        } else {
+                            return Mono.just(new LoginResponseDTO("02", "Error: Autenticación fallida", "", "", "", ""));
+                        }
+
+                    });
+
+        } catch(Exception e) {
+
+            System.out.println(e.getMessage());
+            return Mono.just(new LoginResponseDTO("99", "Error: Ocurrió un problema en la autenticación", "", "", "", ""));
+
+        }
+
+    }
+    @PostMapping("/logout-async")
+    public Mono<LogoutResponseDTO> cerrarSesion(@RequestBody LogoutRequestDTO logoutRequestDTO) {
+        try {
+            return webClientAutenticacion.post()
+                    .uri("/logout")
+                    .body(Mono.just(logoutRequestDTO), LoginRequestDTO.class)
+                    .retrieve()
+                    .bodyToMono(LoginResponseDTO.class)
+                    .flatMap(response -> {
+                        if (response.codigo().equals("00")) {
+                            return Mono.just(new LogoutResponseDTO("00", "Cierre de sesión exitoso"));
+                        } else {
+                            return Mono.just(new LogoutResponseDTO("02", "Error al cerrar sesión"));
+                        }
+                    });
+        } catch (Exception e) {
+            return Mono.just(new LogoutResponseDTO("99", "Error en el proceso de cierre de sesión"));
+        }
+    }
+
+
+}
